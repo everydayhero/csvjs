b701e06032e351948b0180a96b989f1bcc44f811
Marty Zalega <marty@zalega.me>
2013-01-22 18:14:49 +1000

    Implemented CSV string parsing

--- csv.js
+++ csv.js
@@ -28,18 +28,37 @@
       arr.forEach(callback);
     } else {
       for (i in arr) {
-        if (arr.hasOwnProperty(i)) {
-          callback(arr[i], i, arr);
+        if (arr.hasOwnProperty(i) && callback(arr[i], i, arr) === false) {
+          break;
         }
       }
     }
   };
 
   var strCount = function(str, char) {
-    var count = 0, i = 0;
-    while ((i = str.indexOf(char, i + 1)) !== -1) { ++count; }
-    return count;
+    return str.split(char).length - 1;
   };
+
+  var mapProperties = function(arr, map) {
+    forEach(map, function(prop, i) {
+      if (typeof arr[prop] === 'undefined') {
+        arr[prop] = arr[i];
+      }
+    });
+  };
+
+  var _converters = {},
+      convert = function(val) {
+        var returnValue = val;
+        forEach(_converters, function(obj) {
+          if (obj.re.test(val)) {
+            returnValue = obj.func(val);
+            return false;
+          }
+        });
+
+        return returnValue;
+      };
 
   var CSV = {
     defaults: {
@@ -51,10 +70,79 @@
       skipBlanks: false,
       forceQuotes: false
     },
+    converters: {
+      '\\d': function(str) {
+        return str.indexOf('.') !== -1 ? parseFloat(str) : parseInt(str);
+      },
+      '(true|TRUE|false|FALSE)': function(str) {
+        return str.toLowerCase() === 'true';
+      }
+    },
     parse: function(str, opts) {
       opts = extend({}, this.defaults, opts);
 
+      var escColSep = RegExp.escape(opts.colSep),
+          escRowSep = RegExp.escape(opts.rowSep),
+          reColSplitter = new RegExp(['(?=', escColSep, ')'].join(''), 'g'),
+          reRowSplitter = new RegExp(['(?=', escRowSep, ')'].join(''), 'g'),
+          escQuoteChar = RegExp.escape(opts.quoteChar),
+          reRemoveQuote = new RegExp(['^\s*', escQuoteChar, '([^', escQuoteChar, ']*)', escQuoteChar, '\s*$'].join('')),
+          reRemoveSplitters = new RegExp('^(' + [escColSep, escRowSep].join('|') + ')'),
+          reDoubleQuote = new RegExp(escQuoteChar + escQuoteChar, 'g'),
+          rows = str.split(reRowSplitter),
+          headerRow = null,
+          arr = [], strBuf = null, colBuf = [];
 
+      forEach(this.converters, function(callback, re) {
+        _converters[re] = {
+          re: new RegExp(re),
+          func: callback
+        };
+      });
+
+      forEach(rows, function(row) {
+        var cols = row.split(reColSplitter);
+
+        forEach(cols, function(col, i) {
+          // Append to buffer
+          strBuf = [strBuf, col].join('');
+          // Check if we have an odd amount of quote characters
+          if (strCount(strBuf, opts.quoteChar) % 2 !== 0) {
+            return;
+          }
+
+          // Remove the column/row separator
+          strBuf = strBuf.replace(reRemoveSplitters, '');
+
+          var match = strBuf.match(reRemoveQuote);
+          // Strip out strings which are quoted
+          if (match) { strBuf = match[1]; }
+          // Unescape double-quotes
+          strBuf = strBuf.replace(reDoubleQuote, opts.quoteChar);
+
+          colBuf.push(convert(strBuf));
+          strBuf = null;
+        });
+
+        // Check if buffer has been flushed
+        if (strBuf === null) {
+          if (!opts.headers || headerRow) {
+            if (opts.headers && headerRow) {
+              mapProperties(colBuf, headerRow);
+            }
+            arr.push(colBuf);
+          } else {
+            headerRow = colBuf;
+          }
+          colBuf = [];
+        }
+      });
+
+    if (strBuf !== null) {
+      throw "Malformed CSV data";
+    }
+
+      return arr;
     },
     stringify: function(csv, opts) {
       opts = extend({}, this.defaults, opts);
--- test/tests.js
+++ test/tests.js
@@ -1,53 +1,133 @@
-var DATA;
+var ARRAY, STR;
+
+function setup() {
+  STR = "Name,Location\nBatman,Gotham City\nSuperman,Metropolis";
+  ARRAY = [
+    ['Name', 'Location'],
+    ['Batman', 'Gotham City'],
+    ['Superman', 'Metropolis']
+  ];
+}
+
+module("CSV parsing", {
+  setup: setup
+});
+
+test("parses a string", function() {
+  var expected = ARRAY,
+      actual = CSV.parse(STR);
+
+  deepEqual(actual, expected);
+});
+
+test("unescapes quotes", function() {
+  var expected = ARRAY,
+      actual = CSV.parse("Name,Location\nBatman,\"Gotham City\"\nSuperman,Metropolis");
+
+  deepEqual(actual, expected);
+});
+
+test("unescapes double quotes", function() {
+  var expected = [['Harvey "Two-face" Dent']],
+      actual = CSV.parse('Harvey ""Two-face"" Dent');
+
+  deepEqual(actual, expected);
+});
+
+test("parse string when column separator is surrounded by quotes", function() {
+  var expected = [['Joker', 'Psycho,Funny,Deadly']],
+      actual = CSV.parse('Joker,"Psycho,Funny,Deadly"');
+
+  deepEqual(actual, expected);
+});
+
+test("parse string when row separator is surrounded by quotes", function() {
+  var expected = [['Joker', 'Psycho\nFunny\nDeadly']],
+      actual = CSV.parse('Joker,"Psycho\nFunny\nDeadly"');
+
+  deepEqual(actual, expected);
+});
+
+test("skip first row when instructed as header", function() {
+  var expected = [['Batman', 'Gotham City'], ['Superman', 'Metropolis']],
+      actual = CSV.parse(STR, {headers: true});
+
+  deepEqual(actual, expected);
+});
+
+test("map field names on rows when headers is enabled", function() {
+  var csv = CSV.parse(STR, {headers: true}),
+      row = csv[0];
+
+  equal(row.Name, 'Batman');
+  equal(row.Location, 'Gotham City');
+});
+
+test("throw error when csv is malformed", function() {
+  throws(function() {
+    CSV.parse('Why so ",serious?');
+  });
+});
+
+module("CSV converters");
+
+test("fixed numbers", function() {
+  deepEqual(CSV.parse("123"), [[123]]);
+  deepEqual(CSV.parse("-123"), [[-123]]);
+});
+
+test("real numbers", function() {
+  deepEqual(CSV.parse("123.12"), [[123.12]]);
+  deepEqual(CSV.parse("-123.12"), [[-123.12]]);
+});
+
+test("booleans", function() {
+  deepEqual(CSV.parse("true"), [[true]]);
+  deepEqual(CSV.parse("false"), [[false]]);
+});
 
 module("CSV stringify", {
-  setup: function() {
-    DATA = [
-      ['Name', 'Location'],
-      ['Batman', 'Gotham City'],
-      ['Superman', 'Metropolis']
-    ];
-  }
+  setup: setup
 });
 
 test("serialize an array", function() {
   var expected = "Name,Location\nBatman,Gotham City\nSuperman,Metropolis",
-      actual = CSV.stringify(DATA);
+      actual = CSV.stringify(ARRAY);
 
   equal(actual, expected);
 });
 
 test("`forceQuote` quotes all fields", function() {
   var expected = "\"Name\",\"Location\"\n\"Batman\",\"Gotham City\"\n\"Superman\",\"Metropolis\"",
-      actual = CSV.stringify(DATA, {forceQuotes: true});
+      actual = CSV.stringify(ARRAY, {forceQuotes: true});
 
   equal(actual, expected);
 });
 
 test("`quoteChar` sets the quote character used", function() {
   var expected = "#Name#,#Location#\n#Batman#,#Gotham City#\n#Superman#,#Metropolis#",
-      actual = CSV.stringify(DATA, {forceQuotes: true, quoteChar: '#'});
+      actual = CSV.stringify(ARRAY, {forceQuotes: true, quoteChar: '#'});
 
   equal(actual, expected);
 });
 
 test("`fieldSizeLimit` truncates fields that are longer in length", function() {
   var expected = "Nam,Loc\nBat,Got\nSup,Met",
-      actual = CSV.stringify(DATA, {fieldSizeLimit: 3});
+      actual = CSV.stringify(ARRAY, {fieldSizeLimit: 3});
 
   equal(actual, expected);
 });
 
 test("`colSep` sets the column separator", function() {
   var expected = "Name|Location\nBatman|Gotham City\nSuperman|Metropolis",
-      actual = CSV.stringify(DATA, {colSep: '|'});
+      actual = CSV.stringify(ARRAY, {colSep: '|'});
 
   equal(actual, expected);
 });
 
 test("`rowSep` sets the row separator", function() {
   var expected = "Name,Location\rBatman,Gotham City\rSuperman,Metropolis",
-      actual = CSV.stringify(DATA, {rowSep: "\r"});
+      actual = CSV.stringify(ARRAY, {rowSep: "\r"});
 
   equal(actual, expected);
 });
